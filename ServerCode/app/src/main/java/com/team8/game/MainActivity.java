0641aa8f5561a703156df79774baf654cd9ed1a4
Ayush <ayushnvijay@gmail.com>
2016-03-04 12:47:53 -0500

    Achievements

--- ServerCode/app/src/main/java/com/team8/game/MainActivity.java
+++ ServerCode/app/src/main/java/com/team8/game/MainActivity.java
@@ -1,15 +1,71 @@
 package com.team8.game;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity  {
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.games.Games;
+import com.google.example.games.basegameutils.BaseGameActivity;
+import com.google.example.games.basegameutils.BaseGameUtils;
+
+public class MainActivity extends BaseGameActivity implements View.OnClickListener,GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViewById(R.id.sign_in_button).setOnClickListener(this);
+        findViewById(R.id.sign_out_button).setOnClickListener(this);
     }
 
 
+    @Override
+    public void onSignInFailed() {
+        findViewById(R.id.sign_in_button).setVisibility(View.VISIBLE);
+        findViewById(R.id.sign_out_button).setVisibility(View.GONE);
+    }
+
+    @Override
+    public void onSignInSucceeded() {
+        findViewById(R.id.sign_in_button).setVisibility(View.GONE);
+        findViewById(R.id.sign_out_button).setVisibility(View.VISIBLE);
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.sign_in_button) {
+            Log.i("11111", "111111");
+            beginUserInitiatedSignIn();
+            Log.i("22222", "22222");
+        }
+        else if (v.getId() == R.id.sign_out_button) {
+            signOut();
+            findViewById(R.id.sign_in_button).setVisibility(View.VISIBLE);
+            findViewById(R.id.sign_out_button).setVisibility(View.GONE);
+        }
+        else if (v.getId() == R.id.show_achievements){
+            startActivityForResult(Games.Achievements.getAchievementsIntent(
+                    getApiClient()), 1);
+        }
+
+    }
+
+    @Override
+    public void onConnected(Bundle bundle) {
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+        BaseGameUtils.resolveConnectionFailure(this,getApiClient(),connectionResult,1,"yayy");
+    }
 }
