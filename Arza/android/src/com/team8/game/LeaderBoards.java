d384e246fc38b6d9a0399effdd8f9a37ab80645e
Ayush <ayushnvijay@gmail.com>
2016-04-08 11:47:23 -0400

    Added java-interface for leaderboard

---
+++ Arza/android/src/com/team8/game/LeaderBoards.java
@@ -0,0 +1,13 @@
+package com.team8.game;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class LeaderBoards extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_leader_boards);
+    }
+}
