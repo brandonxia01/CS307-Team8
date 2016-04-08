c896cdac86bd2157a83e66ad0d683d0a97421121
Ayush <ayushnvijay@gmail.com>
2016-04-08 12:33:28 -0400

    Added OkHttp dependency

--- Arza/android/src/com/team8/game/LeaderBoards.java
+++ Arza/android/src/com/team8/game/LeaderBoards.java
@@ -1,6 +1,7 @@
 package com.team8.game;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 
 public class LeaderBoards extends Activity {
@@ -9,5 +10,9 @@ public class LeaderBoards extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_leader_boards);
+        Intent getIntent = getIntent();
+        
+
+
     }
 }
